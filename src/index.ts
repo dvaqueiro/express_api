2c1fba997300a125da0a5372a05459cc394401ee
dvaqueiro <dvaqueiro@boardfy.com>
2024-02-26 19:39:19 +0100

    dockerize project

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ app.listen(4000, () => {
   console.log(`server running on port 4000`);
 });
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
 
